75bda1d355315614944426949f493c7c073a7dcb
Gupta, Harsh (US - Bengaluru) <harshgupta9@deloitte.com>
2018-10-15 15:10:03 +0530

    - fixing the animation and exports after namespacing

--- plugin.js
+++ plugin.js
@@ -7,13 +7,25 @@ function plugin(opts) {
   opts = opts || {}
   var id = opts.id || 'style'
   var local = '.' + opts.rootClass || ':local(.' + id + ')'
+  var regex = new RegExp('^\\d+%');
   return function(root) {
     return root.walkRules(function(rule) {
       if (rule.selector.substr(0, 7) === ':global') return;
 
       rule.selectors = rule.selectors.map(selector => {
-        const descendant = ':global ' + local + ' ' + selector;
-        const combined = ':global ' + selector.replace(/([\w-\.\#\*]+)(.*)/, '$1' + local + '$2')
+        let descendant;
+
+        if (selector.indexOf(':export') !== -1) {
+          descendant = selector;
+        } else {
+          descendant = local + ' ' + selector;
+        }
+
+        if (descendant.split(' ')[1] === 'from' || descendant.split(' ')[1] == 'to' || regex.test(descendant.split(' ')[1])) {
+          descendant = selector;
+        }
+
+        const combined = ':global ' + selector.replace(/([\w-\.\#\*]+)(.*)/, '$1' + local + '$2') 
 
         if (opts.combine && opts.descendant) {
           return [combined, descendant].join(', ');
